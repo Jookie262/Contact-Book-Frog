fabfa7cf82554d832be4e86d213070bbe47e9203
Jookie262 <barrientos.134038@iloilo.sti.edu.ph>
2021-11-22 19:06:47 +0800

    Set UserInterface.java to run in the main class

--- src/contactbookfrog/ContactBookFrog.java
+++ src/contactbookfrog/ContactBookFrog.java
@@ -15,6 +15,7 @@ public class ContactBookFrog {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new UserInterface().setVisible(true);
     }
     
 }
